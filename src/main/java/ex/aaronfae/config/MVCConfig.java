0a23e08769acb1cc26ca77937b83e35f6a465ceb
AaronFae <AaronFae@163.com>
2018-03-17 19:49:49 +0800

    4.4 Spring MVC 基本配置

--- src/main/java/ex/aaronfae/config/MVCConfig.java
+++ src/main/java/ex/aaronfae/config/MVCConfig.java
@@ -3,10 +3,7 @@ package ex.aaronfae.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -38,5 +35,15 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(demoInterceptor());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false);
     }
 }
---
+++ src/main/resources/views/error.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AaronFae
+  Date: 2018/3/17
+  Time: 19:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@taglib uri="http://java.sun.com/jstl/core" prefix="c" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>error</title>
+</head>
+<body>
+${errorMessage}
+</body>
+</html>
